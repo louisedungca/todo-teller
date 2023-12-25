d395a23d31a8e7a223bb3cc7ed09f14754a28f0d
Louise Dungca <dungca.louisechristine@gmail.com>
2023-12-25 20:21:48 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <main className='h-screen w-full flex justify-center items-center flex-col gap-3'>
+      <h1 className='text-[1.5rem] font-bold'>
+        Welcome to my new project.
+      </h1>
+      <small>
+        This is a random to-do picker using ReactJS + Vite + Tailwind CSS.
+      </small>
+    </main>    
+  )
+}

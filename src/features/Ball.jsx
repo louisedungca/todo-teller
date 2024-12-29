c160a8342d53111e17c7f4b518a1211ec35eb297
Louise Dungca <louise.dungca@forwardlabs.ai>
2024-12-29 12:46:25 +0800

    fix: change ui display when changing isFocus

--- src/features/Ball.jsx
+++ src/features/Ball.jsx
@@ -1,62 +1,84 @@
-import gsap from 'gsap';
-import { useState } from 'react';
-import { setLocalStorage } from '../utils';
+import gsap from "gsap";
+import { useEffect, useState } from "react";
+import { setLocalStorage } from "../utils";
 
-function Ball({ todos, setTodos, setFocusedTodo, setCompleteTask }) {
+function Ball({
+  todos,
+  setTodos,
+  setFocusedTodo,
+  completeTask,
+  setCompleteTask,
+  setIsLoading,
+}) {
   const [response, setResponse] = useState(null);
   const [isShaking, setIsShaking] = useState(true);
 
   function getRandomTodo() {
     setIsShaking(true);
+    setIsLoading(true);
     setCompleteTask(false);
-    const incompleteTodos = todos.filter(todo => !todo.isCompleted);
+    const incompleteTodos = todos.filter((todo) => !todo.isCompleted);
 
-    gsap.to('#ball-outer', { 
-      x: 5, 
+    gsap.to("#ball-outer", {
+      x: 5,
       y: 2.5,
       rotate: 15,
-      ease: 'wiggle', 
-      duration: 0.5, 
-      repeat: 3, 
+      ease: "wiggle",
+      duration: 0.5,
+      repeat: 3,
       yoyo: true,
     });
 
-    if (incompleteTodos.length > 0) { 
+    if (incompleteTodos.length > 0) {
       const randomIndex = Math.floor(Math.random() * incompleteTodos.length);
       const selectedTodo = incompleteTodos[randomIndex];
-  
-      setTodos((prevTodos) => {
-        const updatedTodos = prevTodos.map((todo) =>
-          todo.id === selectedTodo.id
-            ? { ...todo, isFocus: true }
-            : { ...todo, isFocus: false }
-        );
-        setLocalStorage('todolist', updatedTodos);
-        setFocusedTodo([selectedTodo]); 
-        return updatedTodos;
-      });
-  
+
       setTimeout(() => {
+        setTodos((prevTodos) => {
+          const updatedTodos = prevTodos.map((todo) =>
+            todo.id === selectedTodo.id
+              ? { ...todo, isFocus: true }
+              : { ...todo, isFocus: false }
+          );
+          setLocalStorage("todolist", updatedTodos);
+          setFocusedTodo([selectedTodo]);
+          return updatedTodos;
+        });
+
         setResponse(selectedTodo.task);
         setIsShaking(false);
+        setIsLoading(false);
       }, 2000);
-    }  
+    }
   }
 
+  useEffect(() => {
+    if (completeTask) {
+      setResponse(null);
+      setIsShaking(true);
+    }
+  }, [completeTask]);
+
   // wiggle animation
-  gsap.registerEase('wiggle', (progress) => {
+  gsap.registerEase("wiggle", (progress) => {
     return Math.sin(progress * Math.PI * 3);
   });
 
   return (
-    <div 
+    <div
       className='cursor-pointer relative flex justify-center items-center bg-[#111] w-ball__outer aspect-square rounded-full shadow-ball__outer m-[3rem]'
       id='ball-outer'
       onClick={getRandomTodo}
     >
       <div className='flex justify-center items-center bg-[#F5F5F5] w-ball__inner aspect-square rounded-full shadow-ball__inner p-1 z-50'>
-        <h3 className={`text-black text-center font-medium p-1 ${isShaking ? 'text-font8' : 'text-fontResponse'}`}>
-          {isShaking ? '8' : response }
+        <h3
+          className={`p-1 text-black text-center font-medium ${
+            isShaking
+              ? "text-font8"
+              : "text-fontResponse p-0.5 max-w-ball__inner aspect-square overflow-y-auto scrollbar-none"
+          }`}
+        >
+          {isShaking ? "8" : response}
         </h3>
       </div>
       <div className='absolute top-0 bg-[#333] opacity-20 w-ball__spotlight aspect-square rounded-full'></div>
--- src/features/todo/TodoWrapper.jsx
+++ src/features/todo/TodoWrapper.jsx
@@ -1,14 +1,25 @@
-import { ArchiveBoxArrowDownIcon, PencilSquareIcon } from '@heroicons/react/24/solid';
-import { useEffect, useState, useRef } from 'react';
-import { v4 as randomuuid } from 'uuid'; 
-import { deleteItem, setLocalStorage } from '../../utils';
-import * as global from '../../components';
-import * as c from './components';
+import {
+  ArchiveBoxArrowDownIcon,
+  PencilSquareIcon,
+} from "@heroicons/react/24/solid";
+import { useEffect, useState, useRef } from "react";
+import { v4 as randomuuid } from "uuid";
+import { deleteItem, setLocalStorage } from "../../utils";
+import * as global from "../../components";
+import * as c from "./components";
 
-function TodoWrapper({ todos, setTodos, focusedTodo, setFocusedTodo, completeTask, setCompleteTask }) {
+function TodoWrapper({
+  todos,
+  setTodos,
+  focusedTodo,
+  completeTask,
+  setCompleteTask,
+  isLoading,
+}) {
   const [editTaskId, setEditTaskId] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
   const inputRef = useRef(null);
+
+  const todoList = todos.filter((todo) => !todo.isCompleted && !todo.isFocus);
 
   function addTodo(task) {
     if (editTaskId) {
@@ -16,7 +27,7 @@ function TodoWrapper({ todos, setTodos, focusedTodo, setFocusedTodo, completeTas
         const editedTodos = prevTodos.map((todo) =>
           todo.id === editTaskId ? { ...todo, task, isEditing: false } : todo
         );
-        setLocalStorage('todolist', editedTodos);
+        setLocalStorage("todolist", editedTodos);
         return editedTodos;
       });
       setEditTaskId(null);
@@ -37,9 +48,9 @@ function TodoWrapper({ todos, setTodos, focusedTodo, setFocusedTodo, completeTas
   function deleteTodo(id) {
     setTodos((prevTodos) => {
       const newTodos = prevTodos.filter((todo) => todo.id !== id);
-      deleteItem({ 
-        key: 'todolist', 
-        id 
+      deleteItem({
+        key: "todolist",
+        id,
       });
       return newTodos;
     });
@@ -53,7 +64,7 @@ function TodoWrapper({ todos, setTodos, focusedTodo, setFocusedTodo, completeTas
       const focusedTask = newTodos.find((todo) => todo.id === id);
 
       setCompleteTask(focusedTask?.isCompleted || false);
-      setLocalStorage('todolist', newTodos);
+      setLocalStorage("todolist", newTodos);
       return newTodos;
     });
   }
@@ -76,7 +87,7 @@ function TodoWrapper({ todos, setTodos, focusedTodo, setFocusedTodo, completeTas
   };
 
   useEffect(() => {
-    setLocalStorage('todolist', todos);
+    setLocalStorage("todolist", todos);
   }, [todos, setTodos]);
 
   useEffect(() => {
@@ -85,125 +96,121 @@ function TodoWrapper({ todos, setTodos, focusedTodo, setFocusedTodo, completeTas
     }
   }, [editTaskId]);
 
-  useEffect(() => {
-    if (focusedTodo.length > 0) {
-      setIsLoading(true);
-
-      const focusTimeout  = setTimeout(() => {
-        setIsLoading(false);        
-      }, 2000);
-
-      return () => clearTimeout(focusTimeout);
-    }
-  }, [focusedTodo, setFocusedTodo]);
-
   return (
     <div className='flex flex-col gap-3'>
-      <h3 className='p-1 text-center uppercase'>
-        to do list
-      </h3>
+      <h3 className='p-1 text-center uppercase'>To Do List</h3>
       <c.TodoForm
         todos={todos}
         addTodo={addTodo}
-        editTask={editTaskId ? todos.find((todo) => todo.id === editTaskId) : null}
+        editTask={
+          editTaskId ? todos.find((todo) => todo.id === editTaskId) : null
+        }
         inputRef={inputRef}
         onBlur={handleBlur}
       />
 
-      <div className='w-[100%] flex flex-col gap-2 px-2'>
-        {isLoading ? (
-          <div className='w-[100%] flex flex-col gap-2'>
-            <small className='capitalize'>Focus</small>
-            <div className='flex justify-between items-center bg-white text-black p-1 rounded-md w-1/6 h-8'>
-              <span className='w-full px-1 text-sm'>
-                <global.EllipsisLoader />
-              </span>
-            </div>
-            {todos.every((todo) => todo.isCompleted) ? null : (<small className='capitalize pt-2'>Next</small>)}
-          </div>
-        ) : (
-          <div className='w-[100%] flex flex-col gap-2'>
-            <small className='capitalize'>Focus</small>
-            {focusedTodo.map((todo, idx) => (
-              <div
-                key={idx}
-                className={`flex justify-between items-center h-8 bg-white text-black p-1 rounded-md outline-teal-500 outline-4 outline`}
-              >
-                <span
-                  className={`cursor-pointer w-[80%] px-1 text-sm ${
-                    (todo.isCompleted || completeTask) ? 'line-through text-gray-500' : ''
-                  }`}
-                  onClick={() => {
-                    toggleCompletion(todo.id);
-                  }}
-                >
-                  {todo.isCompleted || completeTask ? (
-                    <span className='line-through text-gray-500'>{todo.task}</span>
-                  ) : (
-                    <span>{todo.task}</span>
-                  )}
+      {todos.length > 0 && (
+        <div className='w-[100%] flex flex-col gap-2 px-2'>
+          {isLoading ? (
+            <div className='w-[100%] flex flex-col gap-2'>
+              <small className='capitalize'>Focus</small>
+              <div className='flex justify-between items-center bg-white text-black p-1 rounded-md w-1/6 h-fit'>
+                <span className='w-full px-1 text-sm'>
+                  <global.EllipsisLoader />
                 </span>
               </div>
-            ))}
-            {todos.every((todo) => todo.isCompleted) ? null : (<small className='capitalize pt-2'>Next</small>)}
-          </div>
-        )}
-
-        {todos.length > 0 && (
-          <div className='flex flex-col gap-2 p-2 overflow-y-auto max-h-[250px]'>
-            {todos
-            .filter((todo) => !todo.isCompleted && !todo.isFocus)
-            .map((todo) => (           
-              <div
-                key={todo.id}
-                className={`flex justify-between items-center h-8 bg-white text-black p-1 rounded-md`}
+            </div>
+          ) : (
+            <div className='w-[100%] flex flex-col gap-2'>
+              <small
+                className={`${
+                  focusedTodo.length > 0 ? "inline-flex" : "hidden"
+                } capitalize`}
               >
-                {editTaskId === todo.id ? (
-                  <input
-                    ref={inputRef}
-                    value={todo.task}
-                    onChange={(e) =>
-                      setTodos((prevTodos) =>
-                        prevTodos.map((task) =>
-                          task.id === todo.id
-                            ? { ...task, task: e.target.value }
-                            : task
-                        )
-                      )
-                    }
-                    onBlur={handleBlur}
-                    className='w-[80%] px-1'
-                  />
-                ) : (
+                Focus
+              </small>
+              {focusedTodo.map((todo, idx) => (
+                <div
+                  key={idx}
+                  className={`flex justify-between items-center h-fit bg-white text-black p-1 rounded-md outline-teal-500 outline-4 outline`}
+                >
                   <span
                     className={`cursor-pointer w-[80%] px-1 text-sm ${
-                      todo.isCompleted ? 'line-through text-gray-500' : ''
+                      todo.isCompleted || completeTask
+                        ? "line-through text-gray-500"
+                        : ""
                     }`}
-                    onClick={() => toggleCompletion(todo.id)}
+                    onClick={() => {
+                      toggleCompletion(todo.id);
+                    }}
                   >
-                    {todo.task}
+                    {todo.isCompleted || completeTask ? (
+                      <span className='line-through text-gray-500'>
+                        {todo.task}
+                      </span>
+                    ) : (
+                      <span>{todo.task}</span>
+                    )}
                   </span>
-                )}
-                <div className='w-[15%] flex gap-2 justify-around items-center cursor-pointer'>
-                  <PencilSquareIcon
-                    className='hover:opacity-45 w-15'
-                    onClick={() => handleEditClick(todo.id)}
-                  />
-                  <ArchiveBoxArrowDownIcon
-                    className='hover:opacity-45 w-15'
-                    onClick={() => deleteTodo(todo.id)}
-                  />
                 </div>
-              </div>
-            ))}
-          </div>
-        )}
-      </div>
+              ))}
+            </div>
+          )}
 
-      <c.CompletedTasks 
-        todos={todos} 
-        setTodos = {setTodos}
-      />
+          {todoList.length > 0 && (
+            <>
+              <small className='pt-2'>What&apos;s left for me</small>
+              <div className='flex flex-col gap-2 p-2 overflow-y-auto max-h-[250px]'>
+                {todoList.map((todo) => (
+                  <div
+                    key={todo.id}
+                    className={`flex justify-between items-center h-fit bg-white text-black p-1 rounded-md`}
+                  >
+                    {editTaskId === todo.id ? (
+                      <input
+                        ref={inputRef}
+                        value={todo.task}
+                        onChange={(e) =>
+                          setTodos((prevTodos) =>
+                            prevTodos.map((task) =>
+                              task.id === todo.id
+                                ? { ...task, task: e.target.value }
+                                : task
+                            )
+                          )
+                        }
+                        onBlur={handleBlur}
+                        className='w-[80%] px-1'
+                      />
+                    ) : (
+                      <span
+                        className={`cursor-pointer w-[80%] px-1 text-sm ${
+                          todo.isCompleted ? "line-through text-gray-500" : ""
+                        }`}
+                        onClick={() => toggleCompletion(todo.id)}
+                      >
+                        {todo.task}
+                      </span>
+                    )}
+                    <div className='w-[15%] mr-2 flex gap-2 justify-center items-center cursor-pointer'>
+                      <PencilSquareIcon
+                        className='hover:opacity-45 w-6'
+                        onClick={() => handleEditClick(todo.id)}
+                      />
+                      <ArchiveBoxArrowDownIcon
+                        className='hover:opacity-45 w-6'
+                        onClick={() => deleteTodo(todo.id)}
+                      />
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </>
+          )}
+        </div>
+      )}
+
+      <c.CompletedTasks todos={todos} setTodos={setTodos} />
     </div>
   );
 }
